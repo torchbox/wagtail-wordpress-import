63cfccd54cd8f2e7c35c8c051b59044fbcb25f88
Nick Moreton <nick.moreton@torchbox.com>
2021-09-07 10:36:14 +0100

    add some documentaion and refactor

--- wagtail_xmlimport/cls/cls.py
+++ wagtail_xmlimport/cls/cls.py
@@ -6,72 +6,153 @@ from lxml import etree
 
 
 class MaxDepthEtree:
-    """calculate the maximum depth of an xml tree
-    by traversing the tree until the root element is reached"""
+    """
+    Calculate the maximum depth of an xml file tree.
+
+    ...
+
+    Traversing the tree until the root element is reached
+
+    Attributes
+    ----------
+    xml : str
+        the raw xml file
+
+    Methods
+    -------
+    get_depth()
+        Returns the calculated max depth of the xml file
+
+    """
 
     def __init__(self, xml):
-        # we return the max_depth
+        """
+        Parameters
+        -----------
+
+        max_depth : int
+            keep track of the deepest so far
+        tree : the xml tree to parse
+
+        """
         self.max_depth = 0
-        # parse the xml file
         self.tree = etree.ElementTree(etree.fromstring(xml))
 
-    def depth(self, elem, level):
+    def _depth(self, elem, level):
+        """
+        Recursive method to count the current depth
+        """
+        
         if level == self.max_depth:
             self.max_depth += 1
-        # recursive call to get the current depth
         for child in elem:
             self.depth(child, level + 1)
 
     def get_depth(self):
+        """
         # returning 1 greater than actual length to work with
-        self.depth(self.tree.getroot(), -1)
-        return self.max_depth + 1
+        """
+        self._depth(self.tree.getroot(), -1)
+        max_depth = self.max_depth + 1
+        
+        return max_depth
 
 
 class PathsToDict:
-    """flatten the xml tree as a dict.
-    The current depth is tracked and when the depth is less than
-    the max_depth we can stop"""
+    """
+    Flatten the xml file tree.
 
-    def __init__(self, xml_root, max_depth, progress=True):
-        self.max_depth = max_depth
-        self.xml_root = xml_root
+    ...
+
+    This is used when running the discovery command. The ouput has no original
+    data and keys are xml tags and values are xml tag attribute names,
+    genrated form the json file
+
+    Attributes
+    ----------
+    xml_file: str
+        the raw string xml file
+
+    Methods
+    -------
+    get_dict()
+        Traverse the xml tree.
+        Check if the max_depth has been reached and if so stop and return
+        a dict that represents all the unique tags of the xml file including
+        attribute names
+
+        Return collections.OrderedDict()
+
+    """
+
+    def __init__(self, xml_file):
+        """
+        Parameters
+        ----------
+
+        current_depth : int
+            to keep track of the current depth during parsing
+        max_depth : int
+            the number of the deepest xml tags (tree)
+        xml_root : lxml.etree
+            the xml parsed and which we loop through
+        raw_tree : str
+            used when converting a tag to str representaion
+
+        """
         self.current_depth = 0
+        self.max_depth = MaxDepthEtree(xml_file)
+        self.xml_root = etree.fromstring(xml_file)
         self.raw_tree = etree.ElementTree(self.xml_root)
-        self.nice_tree = collections.OrderedDict()
-        self.progress = progress
+        # self.nice_tree = collections.OrderedDict()
 
-    def get_path(self, tag):
-        # output a string like, /catalog/book/description
-        # from the tag path
-        return re.sub(r"\[[0-9]+\]", "", self.raw_tree.getpath(tag))
+    @staticmethod
+    def get_path(tag, raw_tree):
+        """
+        Return a string: output a string like, /catalog/book/description
+        from the tag path
+        """
 
-    def set_current_depth(self, path):
-        # how many parts to path used later to determine
-        # if max depth has been reached
+        return re.sub(r"\[[0-9]+\]", "", raw_tree.getpath(tag))
+
+    @staticmethod
+    def set_current_depth(path):
+        """
+        Count the current parts of path
+        e.g. /catalog/book/description = 3
+        """
+
         return len(str(path).strip("/").split("/"))
 
-    def set_attrs(self, tag):
-        # return any attributes of a path whcih may be useful
+    @staticmethod
+    def set_attrs(tag):
+        """
+        Extract any attribute names of a path
+        """
+
         return ",".join([attr for attr in tag.keys()])
 
     def get_dict(self):
-        # looking for if the max_depth has been reached and if so
-        # stop traversing the tree and retun the result
+        """
+        Traverse the xml tree
+        """
+
+        nice_tree = collections.OrderedDict()
+
         for tag in self.xml_root.iter():
             # if self.progress:
             #     print(".")
-            path = self.get_path(tag)
+            path = self.get_path(tag, self.raw_tree)
             current_depth = self.set_current_depth(path)
             max_depth_reached = current_depth == self.max_depth
 
-            if path in self.nice_tree and max_depth_reached:
+            if path in nice_tree and max_depth_reached:
                 break
 
-            if path not in self.nice_tree:
-                self.nice_tree[path] = []
+            if path not in nice_tree:
+                nice_tree[path] = []
 
             if len(tag.keys()) > 0:
-                self.nice_tree[path] = self.set_attrs(tag)
+                nice_tree[path] = self.set_attrs(tag)
 
-        return self.nice_tree
+        return nice_tree
