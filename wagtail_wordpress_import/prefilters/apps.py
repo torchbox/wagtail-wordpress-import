c34827d1ecff04ca931fffd0703b314ba5d87b8d
Nick Moreton <nick.moreton@torchbox.com>
2021-10-05 16:17:56 +0100

    add prefilters module

---
+++ wagtail_wordpress_import/prefilters/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailWordpressImportPrefiltersConfig(AppConfig):
+    label = "wagtail_wordpress_import_prefilters"
+    name = "wagtail_wordpress_import_prefilters"
+    verbose_name = "Wagtail wordpress import prefilters"
