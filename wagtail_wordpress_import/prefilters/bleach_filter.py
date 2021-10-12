60cbd499637768f99c68d593ab72f775cf3f3d60
Nick Moreton <nick.moreton@torchbox.com>
2021-10-12 17:05:20 +0100

    use alternative import for filter_bleach_clean

--- wagtail_wordpress_import/prefilters/bleach_filter.py
+++ wagtail_wordpress_import/prefilters/bleach_filter.py
@@ -1,9 +1,10 @@
 from bleach.sanitizer import Cleaner
+from django.utils.module_loading import import_string
 from wagtail_wordpress_import.prefilters.constants import (
     ALLOWED_ATTRIBUTES,
     ALLOWED_STYLES,
     ALLOWED_TAGS,
-)
+)  #
 
 
 def filter_bleach_clean(html, options=None):
@@ -18,24 +19,38 @@ def filter_bleach_clean(html, options=None):
     and remove `wagtail_wordpress_import.prefilters.bleach_filter.filter_bleach_clean` filter
     """
 
-    allowed_tags = ALLOWED_TAGS()
-    allowed_attributes = ALLOWED_ATTRIBUTES()
-    allowed_styles = ALLOWED_STYLES()
+    CONF_ALLOWED_TAGS = ALLOWED_TAGS
+    if options and options.get("ADDITIONAL_ALLOWED_TAGS"):
+        allowed_tags = import_string(options["ADDITIONAL_ALLOWED_TAGS"])
+        if callable(allowed_tags):
+            CONF_ALLOWED_TAGS = allowed_tags()
+        else:
+            CONF_ALLOWED_TAGS = CONF_ALLOWED_TAGS + options["ADDITIONAL_ALLOWED_TAGS"]
 
-    if options:
-        if options.get("ADDITIONAL_ALLOWED_TAGS"):
-            allowed_tags = ALLOWED_TAGS() + options["ADDITIONAL_ALLOWED_TAGS"]
-
-        if options.get("ADDITIONAL_ALLOWED_ATTRIBUTES"):
-            allowed_attributes = (
-                ALLOWED_ATTRIBUTES() + options["ADDITIONAL_ALLOWED_ATTRIBUTES"]
+    CONF_ALLOWED_ATTRIBUTES = ALLOWED_ATTRIBUTES
+    if options and options.get("ADDITIONAL_ALLOWED_ATTRIBUTES"):
+        allowed_attributes = import_string(options["ADDITIONAL_ALLOWED_ATTRIBUTES"])
+        if callable(allowed_attributes):
+            CONF_ALLOWED_ATTRIBUTES = allowed_attributes()
+        else:
+            CONF_ALLOWED_ATTRIBUTES = (
+                CONF_ALLOWED_ATTRIBUTES + options["ADDITIONAL_ALLOWED_ATTRIBUTES"]
             )
 
-        if options.get("ADDITIONAL_ALLOWED_STYLES"):
-            allowed_styles = ALLOWED_STYLES() + options["ADDITIONAL_ALLOWED_STYLES"]
+    CONF_ALLOWED_STYLES = ALLOWED_STYLES
+    if options and options.get("ADDITIONAL_ALLOWED_STYLES"):
+        allowed_styles = import_string(options["ADDITIONAL_ALLOWED_STYLES"])
+        if callable(allowed_styles):
+            CONF_ALLOWED_STYLES = allowed_styles()
+        else:
+            CONF_ALLOWED_STYLES = (
+                CONF_ALLOWED_STYLES + options["ADDITIONAL_ALLOWED_STYLES"]
+            )
 
     cleaned = Cleaner(
-        tags=allowed_tags, attributes=allowed_attributes, styles=allowed_styles
+        tags=CONF_ALLOWED_TAGS,
+        attributes=CONF_ALLOWED_ATTRIBUTES,
+        styles=CONF_ALLOWED_STYLES,
     )
 
     cleaned_html = cleaned.clean(html)
