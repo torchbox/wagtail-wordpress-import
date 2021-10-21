7bcecd85b36d1dd525030b0324254b249b1303ae
Nick Moreton <nick.moreton@torchbox.com>
2021-10-21 11:15:23 +0100

    add tests to check developer config changes are honered

--- wagtail_wordpress_import/test/tests/test_linebreaks_wp.py
+++ wagtail_wordpress_import/test/tests/test_linebreaks_wp.py
@@ -19,3 +19,16 @@ class TestLinebreaks(TestCase):
     def test_linebreaks_wp(self):
         p_tags = self.soup.findAll("p")
         self.assertEqual(len(p_tags), 7)
+
+    def test_simple_string(self):
+        input = """line 1
+
+        line 2
+
+        line 3"""
+        soup = BeautifulSoup(filter_linebreaks_wp(input), "html.parser")
+        paragraphs = soup.findAll("p")
+
+        self.assertEqual(len(paragraphs), 3)
+        for i in range(len(paragraphs)):
+            self.assertEqual(paragraphs[i].text.strip()[-1], str(i + 1))
