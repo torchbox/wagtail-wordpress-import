a4c18a2509d22d6d4c7a23aa91308d874be41ebc
Nick Moreton <nick.moreton@torchbox.com>
2021-11-13 17:19:07 +0000

    add test to check tags cache during an import

--- wagtail_wordpress_import/test/tests/test_import_hooks.py
+++ wagtail_wordpress_import/test/tests/test_import_hooks.py
@@ -1,14 +1,22 @@
+import os
 from io import StringIO
 from xml.dom import pulldom
+
 from django.test import TestCase, override_settings
+from wagtail.core.models import Page
 from wagtail_wordpress_import.importers.importer_hooks import (
     import_hooks_xml_tags_to_cache,
 )
-from wagtail_wordpress_import.logger import Logger
 from wagtail_wordpress_import.importers.wordpress import WordpressImporter
+from wagtail_wordpress_import.logger import Logger
 from wagtail_wordpress_import.xml_boilerplate import (
     build_xml_stream,
+    generate_temporay_file,
 )
+
+BASE_PATH = os.path.dirname(os.path.dirname(__file__))
+FIXTURES_PATH = BASE_PATH + "/fixtures"
+LOG_DIR = "fakedir"
 
 
 class TestImportHooksOverrideConfig(TestCase):
@@ -84,3 +92,136 @@ class TestImportHooksXmlTagPersisted(TestCase):
         with self.assertRaises(KeyError) as raises:
             error = bar[1]
         self.assertEqual(str(raises.exception), "1")
+
+
+class WordpressImporterTestsCheckXmlTagsNotCached(TestCase):
+    """
+    This tests that the XML tags are not cached during an import because there
+    is no settings.WORDPRESS_IMPORT_HOOKS_TAGS_TO_CACHE
+    """
+
+    fixtures = [
+        f"{FIXTURES_PATH}/dump.json",
+    ]
+
+    def process_import(self, xml_stream):
+        self.importer = WordpressImporter(xml_stream)
+        self.logger = Logger(LOG_DIR)
+        self.importer.run(
+            logger=self.logger,
+            app_for_pages="example",
+            model_for_pages="TestPage",
+            parent_id=2,
+            page_types=["post"],
+            page_statuses=["publish"],
+        )
+        self.parent_page = Page.objects.get(id=2)
+        self.imported_pages = self.parent_page.get_children().all()
+
+    def test_xml_tags_not_cached(self):
+        fragment = """
+        <wp:foo>
+            <wp:foo_id>3</wp:foo_id>
+            <wp:foo_login>rickw</wp:foo_login>
+            <wp:foo_email>rick@example.com</wp:foo_email>
+            <wp:foo_display_name>Rick Wakeman</wp:foo_display_name>
+            <wp:foo_first_name>Rick</wp:foo_first_name>
+            <wp:foo_last_name>Wakeman</wp:foo_last_name>
+        </wp:foo>
+        <item>
+            <title>A title</title>
+            <link>https://www.example.com/a-title</link>
+            <description />
+            <content:encoded />
+            <excerpt:encoded />
+            <wp:post_id>44221</wp:post_id>
+            <wp:post_date>2015-05-21 15:00:31</wp:post_date>
+            <wp:post_date_gmt>2015-05-21 19:00:31</wp:post_date_gmt>
+            <wp:post_modified>2015-05-21 15:00:44</wp:post_modified>
+            <wp:post_modified_gmt>2015-05-21 19:00:44</wp:post_modified_gmt>
+            <wp:comment_status>open</wp:comment_status>
+            <wp:ping_status>closed</wp:ping_status>
+            <wp:post_name>a-title</wp:post_name>
+            <wp:status>publish</wp:status>
+            <wp:post_type>post</wp:post_type>
+        </item>
+        """
+        built_file = generate_temporay_file(
+            build_xml_stream(xml_tags_fragment=fragment).read()
+        )
+
+        self.process_import(built_file)
+
+        # there should be one page imported
+        self.assertEqual(self.imported_pages.count(), 1)
+        self.assertEqual(self.imported_pages.first().title, "A title")
+
+        # assertions for the content of tags_cache is done in
+        #
+        self.assertEqual(len(self.importer.tags_cache.__dict__.keys()), 0)
+
+
+@override_settings(WORDPRESS_IMPORT_HOOKS_TAGS_TO_CACHE=["wp:foo", "wp:bar"])
+class WordpressImporterTestsCheckXmlTagsNotCached(TestCase):
+    """
+    This tests that the XML tags are cached during an import because
+    settings.WORDPRESS_IMPORT_HOOKS_TAGS_TO_CACHE is set
+    """
+
+    fixtures = [
+        f"{FIXTURES_PATH}/dump.json",
+    ]
+
+    def process_import(self, xml_stream):
+        self.importer = WordpressImporter(xml_stream)
+        self.logger = Logger(LOG_DIR)
+        self.importer.run(
+            logger=self.logger,
+            app_for_pages="example",
+            model_for_pages="TestPage",
+            parent_id=2,
+            page_types=["post"],
+            page_statuses=["publish"],
+        )
+        self.parent_page = Page.objects.get(id=2)
+        self.imported_pages = self.parent_page.get_children().all()
+
+    def test_xml_tags_not_cached(self):
+        fragment = """
+        <wp:foo>
+            <wp:foo_id>3</wp:foo_id>
+            <wp:foo_login>rickw</wp:foo_login>
+            <wp:foo_email>rick@example.com</wp:foo_email>
+            <wp:foo_display_name>Rick Wakeman</wp:foo_display_name>
+            <wp:foo_first_name>Rick</wp:foo_first_name>
+            <wp:foo_last_name>Wakeman</wp:foo_last_name>
+        </wp:foo>
+        <item>
+            <title>A title</title>
+            <link>https://www.example.com/a-title</link>
+            <description />
+            <content:encoded />
+            <excerpt:encoded />
+            <wp:post_id>44221</wp:post_id>
+            <wp:post_date>2015-05-21 15:00:31</wp:post_date>
+            <wp:post_date_gmt>2015-05-21 19:00:31</wp:post_date_gmt>
+            <wp:post_modified>2015-05-21 15:00:44</wp:post_modified>
+            <wp:post_modified_gmt>2015-05-21 19:00:44</wp:post_modified_gmt>
+            <wp:comment_status>open</wp:comment_status>
+            <wp:ping_status>closed</wp:ping_status>
+            <wp:post_name>a-title</wp:post_name>
+            <wp:status>publish</wp:status>
+            <wp:post_type>post</wp:post_type>
+        </item>
+        """
+        built_file = generate_temporay_file(
+            build_xml_stream(xml_tags_fragment=fragment).read()
+        )
+
+        self.process_import(built_file)
+
+        # there should be one page imported
+        self.assertEqual(self.imported_pages.count(), 1)
+        self.assertEqual(self.imported_pages.first().title, "A title")
+
+        self.assertEqual(len(self.importer.tags_cache.__dict__.keys()), 2)
