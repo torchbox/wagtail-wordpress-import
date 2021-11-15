592b4bd06704adbade60dd892424fd323d5cc72c
Nick Moreton <nick.moreton@torchbox.com>
2021-11-15 22:46:11 +0000

    add methods for processing cached items per page
    code docs
    
    save
    
    hooks docs
    
    save

--- wagtail_wordpress_import/importers/import_hooks.py
+++ wagtail_wordpress_import/importers/import_hooks.py
@@ -1,33 +1,86 @@
 from django.conf import settings
+from django.utils.module_loading import import_string
 
 
 def import_hooks_xml_items_to_cache():
-    """the xml item tags to cache until the import process ends"""
+    """the xml item tags to cache until the import process ends
+
+    For example:
+    WORDPRESS_IMPORT_HOOKS_ITEMS_TO_CACHE = {
+        "attachment": {
+            "DATA_TAG": "_thumbnail_id",
+            "FUNCTION": "pages.import_hooks.header_image",
+        }
+    }
+    """
     return getattr(settings, "WORDPRESS_IMPORT_HOOKS_ITEMS_TO_CACHE", {})
 
 
 class ItemsCache:
-    """a place to store the WordPress XML item tags
-    These are items tags the don't represent a page to be importred.
-    For example: Images are represented by an <item> tag with a specific
-    <wp:post_type>attachment</wp:post_type>"""
+    """Store the WordPress XML item tags.
+    These are items tags that don't represent a page in the XML file.
+    They are temporarily stored and used at the end of the import process.
+    """
 
     def __init__(self):
-        """based on the values in settings.WORDPRESS_IMPORT_HOOKS_ITEMS_TO_CACHE
-        create the class attributes
+        """For each value in settings.WORDPRESS_IMPORT_HOOKS_ITEMS_TO_CACHE
+        create the class attribute.
 
-        Each class attribute is a list.
-        If one of the config items has a name of "attachment" a class attribute
-        attachment = [] will be created
+        For example:
+        If a config item has a key of "attachment" a class attribute
+        attachment: list would be created
 
-        During the import process all
+        XML fragment example:
         <item>
-        ...
+            <title>foo-item</title>
+            <link>https://www.example.com/foo-item/</link>
+            <pubDate>Tue, 13 Jul 2010 16:16:46 +0000</pubDate>
+            <guid>https://www.example.com/foo.jpg</guid>
+            <wp:post_id>100</wp:post_id>
+            <wp:post_date>2010-07-13 12:16:46</wp:post_date>
+            <wp:post_date_gmt>2010-07-13 16:16:46</wp:post_date_gmt>
+            <wp:post_modified>2010-07-13 16:16:46</wp:post_modified>
+            <wp:post_modified_gmt>2010-07-13 16:16:46</wp:post_modified_gmt>
+            <wp:post_name>foo-item</wp:post_name>
             <wp:post_type>attachment</wp:post_type>
-        ...
         </item>
-        tags would be parsed as a dict and stored in the attachment attribute.
+        would be parsed as a dict
+        {
+            "title": "foo-item",
+            "link": "https://www.example.com/foo-item/",
+            "pubDate": "Tue, 13 Jul 2010 16:16:46 +0000",
+            "guid": "https://www.example.com/foo.jpg",
+            "wp:post_id": 100,
+            "wp:post_date": "2010-07-13 12:16:46",
+            "wp:post_date_gmt": "2010-07-13 16:16:46",
+            "wp:post_modified": "2010-07-13 12:16:46",
+            "wp:post_modified_gmt": "2010-07-13 16:16:46",
+            "wp:post_name": "foo-item",
+            "wp:post_type": "attachment",
+        }
+        and stored in the attachment attribute.
         """
 
         for hook in import_hooks_xml_items_to_cache():
             setattr(self, hook, [])
+
+    def get_cached_items(self):
+        """Return all the cached attributes added from the current config"""
+        return self.__dict__
+
+    def process(self, imported_pages, items_cache):
+        """Run all hooks in the config for each page
+        The function defined in the config key FUNCTION will be imported
+        and run on each page"""
+        for page in imported_pages:
+            for hook in import_hooks_xml_items_to_cache():
+                func, data = self._get_hook_handler_data(items_cache)
+                import_string(func)(page, data, items_cache)
+
+    @staticmethod
+    def _get_hook_handler_data(items_cache):
+        """Get the hook function and data to process"""
+        for hook, actions in import_hooks_xml_items_to_cache().items():
+            if hook in items_cache:
+                return actions["FUNCTION"], actions["DATA_TAG"]
+        return None, None
